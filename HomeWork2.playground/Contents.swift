0771afafe3465f9852c5e5ef3aea0566dd3581df
skripa <shkodnii9999@gmail.com>
2016-05-24 17:02:49 +0300

    HomeWork2

---
+++ HomeWork2.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class TEST {
+    var name: String? = nil
+    var age: Int? = nil
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    func InfoTest() {
+        
+        print(name,age)
+    }
+
+}
+
+///123
